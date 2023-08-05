f1b8a1cb4a40fa3ffe33fd674dcceaad45c83f47
gimhanul <gimhanuly@gmail.com>
2023-08-05 16:06:51 +0900

    initialize

---
+++ src/main/java/com/friends/sickseek/SickseekApplication.java
@@ -0,0 +1,13 @@
+package com.friends.sickseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SickseekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SickseekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/friends/sickseek/SickseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.sickseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SickseekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
