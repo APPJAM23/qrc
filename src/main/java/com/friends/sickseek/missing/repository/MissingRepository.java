b352b7b9f966f8a91969414c3d514f65d0fbabac
gimhanul <gimhanuly@gmail.com>
2023-08-06 00:10:44 +0900

    delete: querydsl

--- src/main/java/com/friends/sickseek/missing/repository/MissingRepository.java
+++ src/main/java/com/friends/sickseek/missing/repository/MissingRepository.java
@@ -3,5 +3,7 @@ package com.friends.sickseek.missing.repository;
 import com.friends.sickseek.missing.domain.Missing;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MissingRepository extends CrudRepository<Missing, Long> {
 }
